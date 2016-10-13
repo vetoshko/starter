4d9b733921a3d4a25a7d541a65388601243df122
wishmaster <wishmaster2310@gmail.com>
2016-10-14 00:36:33 +0500

    + fast add component

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var routes = require('./routes/index');
-var users = require('./routes/users');
+var starter = require('./routes/starter');
 var nunjucks = require('nunjucks');
 
 var app = express();
@@ -28,8 +28,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/starter', starter);
 app.use('/', routes);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
