2b542b06a60072cbe06587262717e865e22def46
vetoshko <vetoshko@jetstyle.ru>
2017-03-29 21:09:34 +0500

    Удален главный слайдер, заменить на скрипт

--- public/javascripts/sources/common.js
+++ public/javascripts/sources/common.js
@@ -26,90 +26,149 @@ $(function() {
         speed: 800,
         slidesToShow: 4,
         slidesToScroll: 4,
-        appendArrows: $('.c-bloggers-wrapper').parents('.c-section__content'),
-        nextArrow: '<div class="slick-next slick-arrow c-bloggers-wrapper__arrow c-bloggers-wrapper__next"></div>',
-        prevArrow: '<div class="slick-prev slick-arrow c-bloggers-wrapper__arrow c-bloggers-wrapper__prev"></div>',
-        dotsClass: 'slick-dots c-bloggers-wrapper__dots'
+        appendArrows: $('.c-bloggers').parents('.c-section__content'),
+        nextArrow: '<div class="slick-next slick-arrow c-bloggers__arrow c-bloggers__next"></div>',
+        prevArrow: '<div class="slick-prev slick-arrow c-bloggers__arrow c-bloggers__prev"></div>',
+        dotsClass: 'slick-dots c-bloggers__dots'
+    }
+    let dreamersConfig = {
+        dots: false,
+        centerMode: false,
+        arrows: true,
+        infinite: false,
+        speed: 500,
+        slidesToShow: 5,
+        appendArrows: $('.c-dreamers').parents('.c-section__content'),
+        nextArrow: '<div class="slick-next slick-arrow c-bloggers__arrow c-bloggers__next"></div>',
+        prevArrow: '<div class="slick-prev slick-arrow c-bloggers__arrow c-bloggers__prev"></div>'
     }
     let cardsPagesCount = 1;
 
     const visibleSymbols = 85;
-
-    // $('body').addClass('hidden-footer');
+    $('body').addClass('hidden-footer');
     $('body').addClass('hidden-footer').animate({
         scrollTop: $('.c-header').height()
     }, 500);
 
-    $('.c-section-container').slick(slickConfig);
-    $('.c-bloggers-wrapper').slick(bloggerConfig);
+    // $('.c-section-container').slick(slickConfig);
+    $('.c-bloggers').slick(bloggerConfig);
     getCards();
     $('#more-cards').click(getCards);
+    $('.c-dreamers').slick(dreamersConfig);
 
-    // let previousWindowOffset = $(window).offset().top;
-    // console.log(previousWindowOffset);
-
+    let sections = $('.c-section-container').find('.c-section');
+    let currentSection;
+    if ($(window).scrollTop() > 0) {
+        currentSection = 1;
+    } else {
+        currentSection = 0;
+    }
     $(window).on('mousewheel DOMMouseScroll MozMousePixelScroll', (event) => {
-        let container = $('.c-section-container');
-        let currentSlide = $('.c-section.slick-active');
-        let currentContent = currentSlide.find('.c-section__content');
         let isScrolledUp = parseInt(event.originalEvent.wheelDelta || -event.originalEvent.detail) >= 0;
-        let heightDelta = Math.floor(currentContent.height() - currentSlide.height());
-        let topOffset = currentSlide.offset().top - $(document).scrollTop();
         if (isScrolledUp) {
-            if (currentSlide.index() > 0) {
-                if (currentContent.offset().top == $('.c-header').height()) {
-                    event.preventDefault();
-                    let currentSlideIdx = container.slick('slickCurrentSlide');
-                    container.slick('slickGoTo', currentSlideIdx - 1);
-                }
-                if (currentSlide.index() == $('.c-section').length - 1) {
-                    if (topOffset == $('.c-footer').height() * -1) {
-                        $('body').animate({
-                            scrollTop: $(document).scrollTop() - $('.c-footer').height()
-                        }, 500);
-                    }
-                }
-            } else {
-                if (currentSlide.find('.c-section__box').scrollTop() <= 0) {
-                        event.preventDefault();
-                        $('body').animate({
-                            scrollTop: 0
-                        }, 500);
-                    }
-            }
+           if (sections.eq(currentSection).find('.c-section__box').position().top == 0) {
+               event.preventDefault();
+               if (currentSection > 0) {
+                   $(window).animate({
+                       scrollTop: sections.eq(currentSection - 1).offset().top
+                   }, 500)
+               }
+           }
         } else {
-            if (topOffset > 0) {
-                event.preventDefault();
-                $('body').animate({
-                    scrollTop: $('.c-header').height()
-                }, 500);
-                return;
-            }
-            if (heightDelta <= currentSlide.find('.c-section__box').scrollTop() || heightDelta < 0) {
-                if (currentSlide.index() < $('.c-section').length) {
-                    let currentSlideIdx = container.slick('slickCurrentSlide');
-                    container.slick('slickGoTo', currentSlideIdx + 1);
-                }
-            } else {
-                if (currentSlide.index() == $('.c-section').length - 1) {
-                    console.log($('.c-footer').offset().top - $(window).scrollTop());
-                    // if (heightDelta - 1 <= currentSlide.find('.c-section__box').scrollTop()) {
-                    //     if (currentSlide.offset().top == $('.c-header').height()) {
-                    //         event.preventDefault();
-                    //         $('body').animate({
-                    //             scrollTop: $(document).scrollTop() + $('.c-footer').height()
-                    //         }, 500);
-                    //     }
-                    // }
-                };
-            }
+            let section = sections.eq(currentSection);
+            let content = section.find('.c-section__box');
+            if (content.position().top == section.height() - content.height()) {
+               event.preventDefault();
+               if (currentSection < sections.length) {
+                   console.log(sections.eq(currentSection + 1).offset().top);
+                   $('body').animate({
+                       scrollTop: sections.eq(currentSection).offset().top
+                   }, 500);
+                   currentSection++;
+               }
+           }
         }
-        setBorders($('.c-section.slick-active'));
     });
+
+
+    // $(window).on('mousewheel DOMMouseScroll MozMousePixelScroll', (event) => {
+    //     let container = $('.c-section-container');
+    //     let currentSlide = $('.c-section.slick-active');
+    //     let currentContent = currentSlide.find('.c-section__content');
+    //     let isScrolledUp = parseInt(event.originalEvent.wheelDelta || -event.originalEvent.detail) >= 0;
+    //     let heightDelta = Math.floor(currentContent.height() - currentSlide.height());
+    //     let topOffset = currentSlide.offset().top - $(document).scrollTop();
+
+    //     if (isScrolledUp) {
+    //         console.log('scrolled up');
+    //         if (currentSlide.index() > 0) {
+    //             console.log('not 1 slide');
+    //             console.log(currentContent.offset().top);
+    //             console.log($('.c-header').height());
+    //             if (currentContent.offset().top == $('.c-header').height()) {
+    //                 event.preventDefault();
+    //                 let currentSlideIdx = container.slick('slickCurrentSlide');
+    //                 container.slick('slickGoTo', currentSlideIdx - 1);
+    //             }
+    //             if (currentSlide.index() == $('.c-section').length - 1) {
+    //                 console.log('last slide');
+    //                 if (topOffset == $('.c-footer').height() * -1) {
+    //                     console.log('hide footer');
+    //                     $('body').animate({
+    //                         scrollTop: $(document).scrollTop() - $('.c-footer').height()
+    //                     }, 500);
+    //                 }
+    //             }
+    //         } else {
+    //             console.log('1 slide');
+    //             if (currentSlide.find('.c-section__box').scrollTop() <= 0) {
+    //                     event.preventDefault();
+    //                     $('body').animate({
+    //                         scrollTop: 0
+    //                     }, 500);
+    //                 }
+    //         }
+    //     } else {
+    //         console.log('scrolled down');    
+    //         console.log(topOffset);        
+    //         if (topOffset > 0) {
+    //             event.preventDefault();
+    //             $('body').animate({
+    //                 scrollTop: $('.c-header').height()
+    //             }, 500);
+    //             return;
+    //         }
+    //         if (heightDelta <= currentSlide.find('.c-section__box').scrollTop() || heightDelta < 0) {
+    //             if (currentSlide.index() < $('.c-section').length - 1) {
+    //                 let currentSlideIdx = container.slick('slickCurrentSlide');
+    //                 container.slick('slickGoTo', currentSlideIdx + 1);
+    //             } else {
+    //                 console.log('footer in da house');
+    //                 event.preventDefault();
+    //                 $('body').animate({
+    //                     scrollTop: $('.c-footer').offset().top + $('.c-footer').height()
+    //                 }, 500);
+    //             }
+    //         } else {
+    //             if (currentSlide.index() == $('.c-section').length - 1) {
+    //                 console.log($('.c-footer').offset().top - $(window).scrollTop());
+    //                 if (heightDelta - 1 <= currentSlide.find('.c-section__box').scrollTop()) {
+    //                     if (currentSlide.offset().top == $('.c-header').height()) {
+    //                         event.preventDefault();
+    //                         $('body').animate({
+    //                             scrollTop: $(document).scrollTop() + $('.c-footer').height()
+    //                         }, 500);
+    //                     }
+    //                 }
+    //             };
+    //         }
+    //     }
+    //     setBorders($('.c-section.slick-active'));
+    // });
 
     function getCards() {
         $.ajax({
-            url: 'http://127.0.0.1:3000/cards',
+            url: 'http://10.66.80.80:3000/cards',
             type: 'GET'
         }).done((cards) => {
             cardsPagesCount++;
